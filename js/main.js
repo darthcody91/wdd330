8cd27eb6a81ecf9a2ee0e66ef97928e72e3d252d
darthcody91 <45984035+darthcody91@users.noreply.github.com>
2022-01-08 19:58:28 -0700

    java update

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const links = [
+    {
+      label: "Week1 notes",
+      url: "week1/index.html"
+    }
+  ]
+         
+function loadIndex() {
+    const ol = document.querySelector("#linkslist");
+    
+    links.forEach( link => {
+        const li = document.createElement("li");
+        const href = document.createElement("a");
+        href.setAttribute("href", li.url);
+        href.innerText = link.label;
+
+        li.appendChild(href);
+        ol.appendChild(link);
+    })
+}
