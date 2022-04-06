43ed27491e230d72732e1b44c87224a477267aea
darthcody91 <45984035+darthcody91@users.noreply.github.com>
2022-04-05 23:56:12 -0600

    more progress

--- finalproject/js/hotwheels.js
+++ finalproject/js/hotwheels.js
@@ -1,103 +1,70 @@
-fetch("json/hotwheels.json")
-    .then((data) => data.json())
-    .then((hotwheels) => {
-        const randomNum = Math.floor(Math.random() * hotwheels.length);
-        const carRandom = hotwheels[randomNum];
+fetch((url = "json/hotwheels.json"))
+  .then((data) => data.json())
+  .then((hotwheels) => {
+    const randomNum = Math.floor(Math.random() * hotwheels.length);
+    const carRandom = hotwheels[randomNum];
 
-    
-        function randomizer() {
-            let random = hotwheels[Math.floor(Math.random() * hotwheels.length)];
-            model.innerText = `${random.model}`;
-            make.innerText = random.make;
-            number.innerText = random.number;
-        }
+    function randomizer() {
+      let random = hotwheels[Math.floor(Math.random() * hotwheels.length)];
+      model.innerText = `${random.model}`;
+      make.innerText = random.make;
+      number.innerText = random.number;
+      model.className = "modelname";
+      make.className = "makename";
+      number.className = "carnumber";
+    }
 
-        randomizer();
+    randomizer();
 
-        document.querySelector("button").addEventListener("click", randomizer);      
-        
-        
+    document.querySelector("button").addEventListener("click", randomizer);
+  });
 
+// this is test code //
 
+fetch(url)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject);
+
+    const hotwheelcars = jsonObject;
+    hotwheelcars.forEach((car) => {
+      let card = document.createElement("dl");
+      let model = document.createElement("dt");
+      let make = document.createElement("dd");
+      let number = document.createElement("dd");
+      let produced = document.createElement("dd");
+      let color = document.createElement("dd");
+
+      make.innerHTML = `${car.make}`;
+      model.innerHTML = `${car.model}`;
+      number.innerHTML = `${car.number}`;
+      produced.innerHTML = `${car.produced}`;
+      color.innerHTML = `${car.color}`;
+
+      card.appendChild(model);
+      card.appendChild(make);
+      card.appendChild(number);
+      card.appendChild(produced);
+      card.appendChild(color);
+
+      document.querySelector("div.cards").appendChild(card);
     });
+  });
 
 
 
- fetch('json/hotwheels.json')
-            .then(function (response) {
-                return response.json();
-            })
-            .then(function (data) {
-                appendData(data);
-            })
-            .catch(function (err) {
-                console.log('error: ' + err);
-            });
-    
-        function appendData(data) {
-           
-            let myTable = document.querySelector('#table');
-
-let headers = ['Make', 'Model', 'Year', 'Color', 'Code'];
-
-
-    let table = document.createElement('table');
-    table.className = "myTable";
-    let headerRow = document.createElement('tr');
-
-    headers.forEach(headerText => {
-        let header = document.createElement('th');
-        let textNode = document.createTextNode(headerText);
-        header.appendChild(textNode);
-        headerRow.appendChild(header);
-    });
-
-    table.appendChild(headerRow);
-
-    data.forEach(cars => {
-        let row = document.createElement('tr');
-
-        Object.values(cars).forEach(text => {
-            let cell = document.createElement('td');
-            let textNode = document.createTextNode(text);
-            cell.appendChild(textNode);
-            row.appendChild(cell);
-        })
-
-        table.appendChild(row);
-    })
-
-    myTable.appendChild(table);
-    
-
-        }
-    
-    ;
 
   
-    const tablebutton = document.getElementById("showcarlist");
-    tablebutton.addEventListener("click", showtable);
-    
+const carbutton = document.getElementById("showrandomcar");
+carbutton.addEventListener("click", showrandom);
 
-    function showtable() {
-        var x = document.getElementById("table");
-        if (x.style.display === "block") {
-          x.style.display = "none";
-        } else {
-          x.style.display = "block";
-        }
-      }
-
-
-      const carbutton = document.getElementById("showrandomcar");
-      carbutton.addEventListener("click", showrandom);
-      
-  
-      function showrandom() {
-          var x = document.getElementById("randomcar");
-          if (x.style.display === "block") {
-            x.style.display = "none";
-          } else {
-            x.style.display = "block";
-          }
-        }
+function showrandom() {
+  var x = document.getElementById("randomcar");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
